f7261015ae1a1aae4b319daa914c12bb74158283
Fabio <90531115+fanaia@users.noreply.github.com>
2026-08-04 16:34:34 -0300

    feat: definir constantes da reconciliação financeira

---
+++ backend/src/services/contasPagar/constants.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  DEFAULT_TIME_ZONE: "America/Sao_Paulo",
+  ETAPA_CONCLUIDO: "Concluído",
+  ETAPA_FATURADO: "Faturado pelo fornecedor",
+  STATUS_ATIVOS: ["Pendente sincronização", "Aberta", "Pagamento cancelado", "Erro"],
+};
