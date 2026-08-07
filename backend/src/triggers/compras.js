1fad631fd33a543956c4a60ab3bcb048309d2a41
Fabio <90531115+fanaia@users.noreply.github.com>
2026-08-07 10:43:02 -0300

    feat: respeitar geração manual após aprovação

--- backend/src/triggers/compras.js
+++ backend/src/triggers/compras.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const { defineTrigger } = require("@oondemand/oon-core-back");
-const { agendarProcessamentoPendentes } = require("../services/contasPagar");
+const { agendarProcessamentoDocumentoOperacional } = require("../services/contasPagar");
 
 defineTrigger("Compra", {
   after: async (document) => {
-    await agendarProcessamentoPendentes(document);
+    await agendarProcessamentoDocumentoOperacional(document);
   },
 });
