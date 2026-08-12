ba1f57bd85c04470cdfe700221df0d9a6745655e
Fabio <90531115+fanaia@users.noreply.github.com>
2026-08-12 07:12:08 -0300

    feat: adiciona permissões RBAC da Tazay

--- backend/src/services/contasPagar/constants.js
+++ backend/src/services/contasPagar/constants.js
@@ -1,5 +1,14 @@
 "use strict";
 
+const TAZAY_PERMISSIONS = Object.freeze({
+  OPERATION_READ: "tazay.operation.read",
+  APPROVAL_EXECUTE: "tazay.approval.execute",
+  FINANCE_READ: "tazay.finance.read",
+  FINANCE_EXECUTE: "tazay.finance.execute",
+  CONFIGURATION_MANAGE: "tazay.configuration.manage",
+  DATA_WRITE: "tazay.data.write",
+});
+
 module.exports = {
   DEFAULT_TIME_ZONE: "America/Sao_Paulo",
   ETAPA_CONCLUIDO: "Concluído",
@@ -12,4 +21,5 @@ module.exports = {
     "Pagamento cancelado",
     "Erro",
   ],
+  TAZAY_PERMISSIONS,
 };
